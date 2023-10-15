5ffddb08a4055e33d8117e03cedd709954cc2873
cedric.d <cedric.dassy73@gmail.com>
2023-10-15 20:56:22 +0200

    test

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,21 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println();
+        System.out.println("Main.main");
         System.out.println("Hello world");
+        for (int i = 0; i < ; i++) {
+
+        }
+
+        if (){
+
+        } else if () {
+
+        }
+        if ( == null) {
+            String 
+            
+        }
     }
 }
